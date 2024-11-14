6242b444ee497dbeae766bad547a37f173aca169
osama <osama@DESKTOP-0R416P1>
2024-11-14 21:21:21 +0200

    Add project files.

---
+++ ChallangeTask/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ChallangeTask
+{
+    internal class Program
+    {
+        static void NewFunction(int first , int second , string opration = "+")
+        {
+            
+            int sum = 0;
+            if (opration == "+")
+            {
+                sum = first + second;
+                Console.WriteLine(sum);
+
+            }
+            else if (opration == "-")
+            {
+                sum = first - second;
+                Console.WriteLine(sum);
+
+            }
+            else if (opration == "*")
+            {
+                sum = first * second;
+                Console.WriteLine(sum);
+
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int firstNum ;
+            int secondNum ;
+            string opreation;
+            Console.Write("Enter first number:");
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number:");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter operation:");
+            opreation = Console.ReadLine();
+            Console.Write("Expected Output:");
+
+            NewFunction(firstNum , secondNum ,opreation);
+
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,133 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var numbers = new List<int>();
+
+            do
+            {
+                Console.WriteLine("P - Print numbers");
+                Console.WriteLine("A - Add a number");
+                Console.WriteLine("M - Display mean of the numbers");
+                Console.WriteLine("S - Display the smallest number");
+                Console.WriteLine("L - Display the largest number");
+                Console.WriteLine("Q - Quit");
+                Console.Write("==>");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "q")
+                {
+                    break;
+                }
+                else if (input.ToLower() == "p")
+                {
+                    if (numbers.Count == 0)
+                    {
+                        Console.WriteLine("List is empty");
+                    }
+                    else
+                    {
+                        for(int i = 0; i < numbers.Count; i++)
+                        {
+                            if (i == 0 && numbers.Count == 1)
+                            {
+                                Console.Write("[" + numbers[i] + "]\n");
+                            }
+                            else if (i == 0)
+                            {
+                                Console.Write("[" + numbers[i]);
+                            }
+                            else if (i == numbers.Count - 1)
+                            {
+                                Console.Write("," + numbers[i] + "]\n");
+                            }
+                            else
+                            {
+                                Console.Write("," + numbers[i]);
+                            }
+                            
+                        }
+                    }
+
+
+                }
+                else if (input.ToLower() == "a")
+                {
+                    Console.Write("Enter a number to add : ==> ");
+                    var number = Convert.ToInt32(Console.ReadLine());
+                    numbers.Add(number);
+                    Console.WriteLine($" {number} added");
+
+
+                }
+                else if (input.ToLower() == "m")
+                {
+                    if (numbers.Count == 0)
+                    {
+                        Console.WriteLine("List is empty");
+                    }
+                    else
+                    {
+                        var sum = 0;
+                        for(int i = 0; i < numbers.Count; i++)
+                        {
+                            sum += numbers[i];
+                        }
+
+                        Console.WriteLine("Mean is " + sum / numbers.Count);
+                    }
+                }
+                else if (input.ToLower() == "s")
+                {
+                    if (numbers.Count == 0)
+                    {
+                        Console.WriteLine("List is empty");
+                    }
+                    else
+                    {
+                        var min = numbers[0];
+                        for(int i = 0; i < numbers.Count; i++)
+                        {
+                            if (numbers[i] < min)
+                            {
+                                min = numbers[i];
+                            }
+                        }
+
+                        Console.WriteLine("Smallest number is " + min);
+                    }
+                }
+                else if (input.ToLower() == "l")
+                {
+                    if (numbers.Count == 0)
+                    {
+                        Console.WriteLine("List is empty");
+                    }
+                    else
+                    {
+                        var max = numbers[0];
+                        for(int i = 0; i < numbers.Count; i++)
+                        {
+                            if (numbers[i] > max)
+                            {
+                                max = numbers[i];
+                            }
+                        }
+                        Console.WriteLine("Largest number is " + max);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            } while (true);
+
+
+
+
+        }
+    }
+}
